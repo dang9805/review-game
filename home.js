d78d913b17cdc194c1f8124ceacc5f57b68c0272
dang9805 <congdang981997@gmail.com>
2021-03-17 22:49:08 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,23 @@
+document.getElementById("minecraft").addEventListener("click",()=>{
+    window.location.href = "minecraft_dungeon.html"
+})
+
+document.getElementById("cs").addEventListener("click",()=>{
+    window.location.href = "csgo.html"
+})
+
+document.getElementById("lol").addEventListener("click",()=>{
+    window.location.href = "lol.html"
+})
+
+document.getElementById("genshin").addEventListener("click",()=>{
+    window.location.href = "genshin.html"
+})
+
+document.getElementById("AboutUs").addEventListener("click",()=>{
+    window.location.href = "about_us.html"
+})
+
+document.getElementById("home").addEventListener("click",()=>{
+    window.location.href = "home.html"
+})
